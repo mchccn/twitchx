ff10cd9ddc1bac184ef5000c74fd04b1e7a9e0ce
Dheeraj Prakash <dheerajpv16@gmail.com>
2021-06-14 13:35:15 -0400

    docs(classes/): added JSDoc

--- src/classes/users/User.ts
+++ src/classes/users/User.ts
@@ -6,60 +6,126 @@ import { ExternalError, HTTPError, snakeCasify, TwitchAPIError } from "../../sha
 import { BASE_URL } from "../../shared/";
 import type { UserData } from "../../types/classes";
 
+/**
+ * Represents a user on twitch.
+ *
+ * @class
+ * @extends Base
+ */
 export default class User extends Base {
+    /**
+     * Creates a user from the given client and
+     * @param client The Client instance that this user belongs to
+     * @param data The raw data provided by the Twitch API
+     *
+     * @constructor
+     */
     public constructor(public readonly client: Client, private data: UserData) {
         super(client);
 
         this.client.emit("userCreate", this);
     }
 
+    /**
+     * The user's ID.
+     * @type {string}
+     */
     public get id() {
         return this.data.id;
     }
 
+    /**
+     * The user's login name (username but all lowercase).
+     * @type {string}
+     */
     public get login() {
         return this.data.login;
     }
 
+    /**
+     * The user's display name.
+     * @type {string}
+     */
     public get displayName() {
         return this.data.display_name;
     }
 
+    /**
+     * The user's type (staff status).
+     * @type {string}
+     */
     public get type() {
         return this.data.type;
     }
 
+    /**
+     * The user's broadcaster type (partner program).
+     * @type {string}
+     */
     public get broadcasterType() {
         return this.data.broadcaster_type;
     }
 
+    /**
+     * Total number of views on this user's channel.
+     * @type {number}
+     */
     public get viewCount() {
         return this.data.view_count;
     }
 
     /**
      * Returns the email of the user (scope `user:read:email` is required).
+     * @type {string|undefined}
      */
     public get email() {
         return this.data.email;
     }
 
+    /**
+     * The user's description.
+     * @type {string}
+     */
     public get description() {
         return this.data.description;
     }
 
+    /**
+     * The Date when the user was created.
+     * @type {Date}
+     */
     public get createdAt() {
         return new Date(this.data.created_at);
     }
 
+    /**
+     * The timestamp of the user being created.
+     * Identical to calling `getTime()` on `createdAt`.
+     * @type {number}
+     */
     public get createdTimestamp() {
         return new Date(this.data.created_at).getTime();
     }
 
-    public avatarURL(options?: { offline?: boolean }) {
+    /**
+     * @typedef {object} AvatarURLOptions
+     * @prop {boolean} offline fetch offline avatar?
+     */
+
+    /**
+     * Returns the user's avatar URL.
+     * If `options.offline` is true, the offline avatar will be returned.
+     * @param {AvatarURLOptions} options options for the avatar URL
+     * @returns {string} the user's avatar URL
+     */
+    public avatarURL(options?: { offline?: boolean }): string {
         return options?.offline ? this.data.offline_image_url : this.data.profile_image_url;
     }
 
+    /**
+     * Updates this user object to hold the newest data.
+     * @returns a promise that should resolve to undefined on success
+     */
     public async update() {
         if (!this.client.options.update.channels) {
             if (!this.client.options.suppressRejections)
@@ -99,6 +165,11 @@ export default class User extends Base {
         return;
     }
 
+    /**
+     * Blocks the user. Requires `user:manage:blocked_users` scope on the client.
+     * @param options the options for blocking the user
+     * @returns A promise that resolves to a boolean, representing the success of the operation
+     */
     public async block(options?: { reason?: "chat" | "whisper"; sourceContext?: "spam" | "harassment" | "other" }) {
         if (!this.client.scope.includes("user:manage:blocked_users"))
             throw new ExternalError(`scope 'user:manage:blocked_users' is required to block users`);
@@ -131,7 +202,11 @@ export default class User extends Base {
         return false;
     }
 
-    public async unblock() {
+    /**
+     * Unblocks the given user. Requires `user:manage:blocked_users` scope on the client.
+     * @returns {Promise<boolean>} a promise that is resolved to a boolean representing the success of the operation
+     */
+    public async unblock(): Promise<boolean> {
         if (!this.client.scope.includes("user:manage:blocked_users"))
             throw new ExternalError(`scope 'user:manage:blocked_users' is required to unblock users`);
 
@@ -152,28 +227,44 @@ export default class User extends Base {
         return false;
     }
 
-    public async fetchBlocks({ first, after }: { first: number, after: string }) {
+    /**
+     * @typedef {object} BlockFetchOptions
+     * @prop {number} first
+     * @prop {string} after
+     */
 
-         const res = await fetch(`${BASE_URL}/users/blocks?${new URLSearchParams(snakeCasify({ broadcaster_id: this.id, after, first }).toString())}`, {
-            headers: {
-                authorization: `Bearer ${this.client.token}`,
-                "client-id": this.client.options.clientId,
-            }, method: 'delete'
-        }).catch((e) => {
+    /**
+     * Returns an array of users that were blocked by this user
+     * @param {BlockFetchOptions} param0 pagination options
+     * @returns {Promise<User[]>} an array of users that this user has blocked
+     */
+    public async fetchBlocks({ first, after }: { first: number; after: string }): Promise<User[]> {
+        const res = await fetch(
+            `${BASE_URL}/users/blocks?${new URLSearchParams(
+                snakeCasify({ broadcaster_id: this.id, after, first }).toString()
+            )}`,
+            {
+                headers: {
+                    authorization: `Bearer ${this.client.token}`,
+                    "client-id": this.client.options.clientId,
+                },
+                method: "delete",
+            }
+        ).catch((e) => {
             throw new HTTPError(e);
         });
 
         if (!res.ok) throw new HTTPError(res.statusText);
 
         const { data }: { data: any[] } = await res.json();
-        
+
         let users: User[] = [];
 
-        data.forEach(u => {
+        data.forEach((u) => {
             let usr = new User(this.client, u);
-            users.push(usr)
+            users.push(usr);
             this.client.users.cache.set(usr.id, usr);
-        })
+        });
 
         return users;
     }
--- src/types/classes/user.ts
+++ src/types/classes/user.ts
@@ -2,8 +2,8 @@ export interface UserData {
     id: string;
     login: string;
     display_name: string;
-    type: string;
-    broadcaster_type: string;
+    type: "staff" | "admin" | "global_mod" | "";
+    broadcaster_type: "partner" | "affiliate" | "";
     description: string;
     profile_image_url: string;
     offline_image_url: string;
